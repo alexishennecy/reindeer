ea32a4a594e39f9a88f0bc55a2daf5606862dfe3
Alexis Hennecy <alexishennecy@users-MacBook-Pro-2.local>
2018-02-28 00:38:25 -0600

    checking that github is updated with current edits to code

--- reindeer.js
+++ reindeer.js
@@ -1,4 +1,4 @@
-// goal is to loop through all the reindeer in the array, and add the name of the reindeer to the single HTML <div> element provided. The name of the reindeer should be prepended with the corresponding color from the other array. meaning combine the colors with the reindeer names as the output.
+// goal is to loop through all the reindeer in the array, and add the name of the reindeer to the single HTML <div> element provided. The name of the reindeer should be prepended with the corresponding color from the other array. meaning combine the colors with the reindeer names as the output..
 
 
 
